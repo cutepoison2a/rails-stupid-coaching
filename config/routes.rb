71c4599d4a6b1ceb18cc0808e9ba4c68b035ab79
joelle colomas <j.colomas81@gmail.com>
2019-05-13 13:47:28 -0400

    coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'questions/ask'
+  get 'questions/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Prefix Verb URI Pattern       Controller#Action
+  #      ask GET  /ask(.:format)    questions#ask
+  #   answer GET  /answer(.:format) questions#answer
+  get '/ask', to: 'questions#ask', as: :ask
+  get '/answer', to: 'questions#answer', as: :answer
 end
